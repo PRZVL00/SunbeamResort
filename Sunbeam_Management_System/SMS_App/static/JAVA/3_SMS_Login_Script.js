57005e65a544e67579cc3c308616348af8e3829e
PRZVL00 <85090090+PRZVL00@users.noreply.github.com>
2022-07-12 07:55:24 +0800

    Delete Sunbeam_Management_System directory

--- Sunbeam_Management_System/SMS_App/static/JAVA/3_SMS_Login_Script.js
+++
@@ -1,9 +0,0 @@
-function validate() {
-    document.forms.submit();
-    }
-
-function cancel() {
-    document.getElementById('username').value = "";
-    document.getElementById('password').value = "";
-    window.location.assign('home')
-}
--- Sunbeam_Management_System/SMS_App/static/JAVA/5_SMS_Confirmation_Script.js
+++
@@ -1,6 +0,0 @@
-function submit_form(){
-    var email = document.getElementById('email').value;
-    alert(email)
-    //document.form.submit();
-}
-
